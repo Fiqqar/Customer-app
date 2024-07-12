8aeccab897cfa473a0f467baa79e13640bbc8232
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-07-12 20:38:57 +0700

    12-7-24 (adding component)
    done
    -adding component
    -fix multi word error

---
+++ frontend/frontend/src/components/Alert.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="alert alert-warning alert-dismissible" role="alert">
+        <button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        {{ message }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Alert",
+    props: ['message']
+}
+</script>
--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -1,3 +1,52 @@
 import Vue from 'vue';
-import App from './App';
+// import App from './App';
 import VueRouter from 'vue-router';
+import vueResource from 'vue-resource';
+import Customers from './components/Customers';
+import Customerdetails from './components/CustomerDetails';
+import Add from './components/Add';
+import Edit from './components/Edit';
+import About from './components/About';
+
+Vue.use(vueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode : "history", 
+    base : __dirname,
+    routes: [
+        {path : "/", component : Customers},
+        {path : "/about", component : About},
+        {path : "/edit/:id", component : Edit},
+        {path : "/add", component : Add},
+        {path : "/details/:id", component : Customerdetails},
+    ]
+});
+
+new Vue({router, template: `
+    <div id="app">
+      <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">vCustomers</a>
+        </div><div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><router-link to="/add">Add Customer</router-link></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+      <router-view></router-view>
+    </div>
+  `
+}).$mount("#app")
